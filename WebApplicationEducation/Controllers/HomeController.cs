f52fc4eb5f52f56aad7408333d0359fe4ab7a5b8
Azizjan Ayupov <litpulla@mail.ru>
2020-11-06 23:04:10 +0600

    Add project files.

---
+++ WebApplicationEducation/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplicationEducation.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplicationEducation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public IActionResult Index()
+        {
+            var users = GetAllUsers();
+
+            return View(users);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private List<User> GetAllUsers()
+        {
+            List<User> result;
+
+            using (IDbConnection db = Connection)
+            {
+                result = db.Query<User>("SELECT * FROM Users").ToList();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ WebApplicationEducation/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebApplicationEducation.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Balance { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
